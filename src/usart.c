a354226585b425508d608b03ae6722ebf426595c
julio <julio@localhost>
2016-11-22 11:50:55 +0100

    Added non-blocking byte-read method

--- src/usart.c
+++ src/usart.c
@@ -7,8 +7,7 @@
 
 #include "usart.h"
 
-void init_eusart()
-{
+void init_eusart() {
     // disable interrupts before changing states
     PIE1bits.RCIE = 0;
     PIE1bits.TXIE = 0;
@@ -30,42 +29,39 @@ void init_eusart()
     //SPBRGL = 16;
     // Baud Rate = 19200; SPBRGL 51;
     SPBRGL = 51;
-    
+
     // Baud Rate = 115200; SPBRGH 0;
     SPBRGH = 0x00;
-    
+
 
     // initializing the driver state
     eusartTxHead = 0;
     eusartTxTail = 0;
-    eusartTxBufferRemaining = sizeof(eusartTxBuffer);
+    eusartTxBufferRemaining = sizeof (eusartTxBuffer);
 
     eusartRxHead = 0;
     eusartRxTail = 0;
     eusartRxCount = 0;
 
     EUSART_PurgeBuffer();
-            
+
     // enable receive interrupt
     PIE1bits.RCIE = 1;
 }
 
 /*
  * Reads 1 Byte from buffer and advances
-*/
-uint8_t EUSART_Read_1Byte(void)
-{
-    uint8_t readValue  = 0;
+ */
+uint8_t EUSART_Read_1Byte(void) {
+    uint8_t readValue = 0;
 
-    while(0 == eusartRxCount)
-    {
+    while (0 == eusartRxCount) {
     }
 
     PIE1bits.RCIE = 0;
 
     readValue = eusartRxBuffer[eusartRxTail++];
-    if(sizeof(eusartRxBuffer) <= eusartRxTail)
-    {
+    if (sizeof (eusartRxBuffer) <= eusartRxTail) {
         eusartRxTail = 0;
     }
     eusartRxCount--;
@@ -74,32 +70,47 @@ uint8_t EUSART_Read_1Byte(void)
     return readValue;
 }
 
-uint16_t EUSART_Read_2Bytes(void)
-{
-	uint16_t readValue = 0;
-	readValue |= EUSART_Read_1Byte();
-	readValue << 8;
-	readValue |= EUSART_Read_1Byte();
-	
-	return readValue;
+/*
+ * Nonblocking - Reads 1 Byte from buffer and advances.
+ */
+uint8_t EUSART_Read_1Byte_NONBL(void) {
+    uint8_t readValue = 0;
+
+    //    while(0 == eusartRxCount)
+    //    {
+    //    }
+    if (1 == eusartRxBuffer) {
+        PIE1bits.RCIE = 0;
+
+        readValue = eusartRxBuffer[eusartRxTail++];
+        if (sizeof (eusartRxBuffer) <= eusartRxTail) {
+            eusartRxTail = 0;
+        }
+        eusartRxCount--;
+        PIE1bits.RCIE = 1;
+    }
+    return readValue;
 }
 
-void EUSART_Write_1Byte(byte txData)
-{
-    while(0 == eusartTxBufferRemaining)
-    {
+uint16_t EUSART_Read_2Bytes(void) {
+    uint16_t readValue = 0;
+    readValue |= EUSART_Read_1Byte();
+    readValue << 8;
+    readValue |= EUSART_Read_1Byte();
+
+    return readValue;
+}
+
+void EUSART_Write_1Byte(byte txData) {
+    while (0 == eusartTxBufferRemaining) {
     }
 
-    if(0 == PIE1bits.TXIE)
-    {
+    if (0 == PIE1bits.TXIE) {
         TXREG = txData;
-    }
-    else
-    {
+    } else {
         PIE1bits.TXIE = 0;
         eusartTxBuffer[eusartTxHead++] = txData;
-        if(sizeof(eusartTxBuffer) <= eusartTxHead)
-        {
+        if (sizeof (eusartTxBuffer) <= eusartTxHead) {
             eusartTxHead = 0;
         }
         eusartTxBufferRemaining--;
@@ -107,42 +118,34 @@ void EUSART_Write_1Byte(byte txData)
     PIE1bits.TXIE = 1;
 }
 
-void EUSART_Write_2Bytes(word txData)
-{
-    EUSART_Write_1Byte((byte)txData);
-    EUSART_Write_1Byte((byte)txData << 8);
+void EUSART_Write_2Bytes(word txData) {
+    EUSART_Write_1Byte((byte) txData);
+    EUSART_Write_1Byte((byte) txData << 8);
 }
 
-void EUSART_WriteCString(const byte* str)
-{
-    while ( *str )
-    {   
-        EUSART_Write_1Byte( *str++ );
+void EUSART_WriteCString(const byte* str) {
+    while (*str) {
+        EUSART_Write_1Byte(*str++);
     }
 }
 
-void EUSART_WriteString(byte* str)
-{
-    while ( *str )
-    {
-        EUSART_Write_1Byte( *str++ );
+void EUSART_WriteString(byte* str) {
+    while (*str) {
+        EUSART_Write_1Byte(*str++);
     }
 }
 
-void EUSART_PurgeBuffer(void)
-{
-//    memset(eusartTxBuffer, 0, sizeof (eusartTxBuffer));
- //   memset(eusartRxBuffer, 0, sizeof (eusartRxBuffer));
+void EUSART_PurgeBuffer(void) {
+    //    memset(eusartTxBuffer, 0, sizeof (eusartTxBuffer));
+    //   memset(eusartRxBuffer, 0, sizeof (eusartRxBuffer));
 }
 
 /**
  *  ISRs
  */
-void EUSART_Receive_ISR(void)
-{
+void EUSART_Receive_ISR(void) {
     // Check for EUSART error - restart
-    if(1 == RCSTAbits.OERR)
-    {
+    if (1 == RCSTAbits.OERR) {
         RCSTAbits.CREN = 0;
         RCSTAbits.CREN = 1;
     }
@@ -150,28 +153,22 @@ void EUSART_Receive_ISR(void)
     // buffer overruns are ignored
     eusartRxBuffer[eusartRxHead++] = RCREG;
 
-    if(sizeof(eusartRxBuffer) <= eusartRxHead)
-    {
+    if (sizeof (eusartRxBuffer) <= eusartRxHead) {
         eusartRxHead = 0;
     }
     eusartRxCount++;
 }
 
-void EUSART_Transmit_ISR(void)
-{
+void EUSART_Transmit_ISR(void) {
 
     // add your EUSART interrupt custom code
-    if(sizeof(eusartTxBuffer) > eusartTxBufferRemaining)
-    {
+    if (sizeof (eusartTxBuffer) > eusartTxBufferRemaining) {
         TXREG = eusartTxBuffer[eusartTxTail++];
-        if(sizeof(eusartTxBuffer) <= eusartTxTail)
-        {
+        if (sizeof (eusartTxBuffer) <= eusartTxTail) {
             eusartTxTail = 0;
         }
         eusartTxBufferRemaining++;
-    }
-    else
-    {
+    } else {
         PIE1bits.TXIE = 0;
     }
 }
