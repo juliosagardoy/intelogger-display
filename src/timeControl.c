ab07c358e67f62e7afe13a1e3a97a3de2c9ee76d
julio <julio@localhost>
2016-11-23 16:21:55 +0100

    Fixed bad CCP PWM module configuration

--- src/timeControl.c
+++ src/timeControl.c
@@ -6,12 +6,10 @@
  */
 
 #include <stdlib.h>
-#include <pic16f1938.h>
-
+#include "Globals.h"
 #include "timeControl.h"
 #include "display.h"
 #include "nmea.h"
-
 
 static time_t uptime_s; /* time_t in seconds, from time.h */
 static volatile byte active_digit; /* Stores below enum 0,1,2,3 active 7-seg digit */
@@ -24,11 +22,12 @@ void
 init_tmr6() {
     active_digit = 0;
 
-    /* Configure overflow: Fcy=1MHz; (Tcy=100ns * 10 * 64)/2 = 38,4us per tick */
-    T6CONbits.T6CKPS = 0b00; // Prescaler @ 1:64
-    T6CONbits.T6OUTPS = 0b1110; // Postscaler @ 1:12
+    /* Configure overflow: Fcy=1MHz; Tcy=1us; incTMR=Tcy/2
+     * 0.5us * 1 * 12 = 6us per tick */
+    T6CONbits.T6CKPS = 0b00; // Prescaler @ 1:1
+    T6CONbits.T6OUTPS = 0b1011; // Postscaler @ 1:12
 
-    TMR6 = 0; // Then irq ea. 256*38,4 us = 9.830 ms = 102Hz
+    TMR6 = 0; // Then irq ea. 256*6us = 15 ms = 65Hz
     PIE3bits.TMR6IE = 1;
     T6CONbits.TMR6ON = 1;
 }
@@ -38,10 +37,11 @@ init_tmr6() {
  */
 static void
 init_tmr4() {
-    /* Configure overflow: Tcy=1/(Fosc/4)= 1us * 64presc = 64us/tick 
-     * 64us/tick * 256 = 0,016384 s to overflow
+    /* Configure overflow:cy=1MHz; Tcy=1us; incTMR=Tcy/2
+     * 0,5us * 64presc = 32us/tick 
+     * 32us/tick * 256 = 0,008192 s to overflow
      */
-    T4CONbits.T4CKPS = 0b01; /* Prescaler @ 1:64 */
+    T4CONbits.T4CKPS = 0b11; /* Prescaler @ 1:64 */
     PIE3bits.TMR4IE = 1;
 
     T4CONbits.TMR4ON = 1;
@@ -54,10 +54,11 @@ void
 init_tmr2() {
     uptime_s = 0x00;
 
-    /* Configure overflow: Fcy=1MHz; Tcy=100ns * 1 * 10 = 1 us per tick */
-    T2CONbits.T2CKPS = 0b00; // Prescaler @ 1:1
+    /* Configure overflow: Fcy=1MHz; Tcy=1us; incTMR=Tcy/2
+     * 0.5us * 64 * 10 = 20 us per tick */
+    T2CONbits.T2CKPS = 0b11; // Prescaler @ 1:64
     T2CONbits.T2OUTPS = 0b1001; // Postscaler @ 1:10
-    TMR2 = 6; // Preload at 6, then irq ea. (2^8-6)*1 us = 250 us
+    TMR2 = 6; // Preload at 6, then irq ea. (2^8-6)*20 us = 5ms
     PIE1bits.TMR2IE = 1;
     T2CONbits.TMR2ON = 1;
 }
@@ -69,14 +70,13 @@ void
 init_ccp() {
     TRISBbits.TRISB0 = 1; /* Temporarily disable output to avoid glitches */
     CCPTMRS0bits.C4TSEL = 0b01; /* CCP4 is based off Timer4 in PWM mode */
-    PR4 = 255; /* PWM Period = 0.016384 s = 62Hz */
+    PR4 = 255; /* PWM Period = TMR2 till ovf = 0.008192 s */
 
-    /* DC will vary, refresh rate fixed to 62Hz */
-    CCP4CON = 0b11; /* Select PWM mode and duty cycle 2-MSb */
+     /* Select duty cycle 2-MSb (10) and PWM mode (11xx) */
+    CCP4CON = 0b101100;
 
-    /* () Remember 2 LSb are unused but specified in CCP4CON */
-    //CCPR4L = 0b111111; /* duty cycle 8-MSb */
-    CCPR4L = 0b100000; /* duty cycle 8-MSb */
+    /* 2 LSb in CCP4CON */
+    CCPR4L = 0b000000; /* duty cycle 8-MSb */
     init_tmr4();
 
     PIE3bits.CCP4IE = 1;
@@ -88,11 +88,15 @@ void incr_uptime(time_t s) {
 }
 
 byte Toggle_Brightess(void) {
-    if (CCPR4L < 0b100000)
+    if (CCPR4L < 0b100000) {
+        CCP4CON = 0b111100;
         CCPR4L = 0b111111; /* Make bright again - DC=100% */
-    else
-        CCPR4L = 0b100000; /* Cut DC to 50% */
-    return CCPR4L;
+        return 1;
+    } else {
+        CCP4CON = 0b101100;
+        CCPR4L = 0b000000; /* Cut DC to 50% */
+        return 0;
+    }
 }
 
 static void incr_active_digit() {
@@ -102,7 +106,7 @@ static void incr_active_digit() {
         active_digit = 0;
 }
 
-/* Overflow every 9.98 ms! 100Hz*/
+/* Overflow every 15 ms! 65Hz*/
 void TMR6_ISR() {
     TMR6 = 0;
 
@@ -132,29 +136,31 @@ void TMR6_ISR() {
     PIR3bits.TMR6IF = 0;
 }
 
-/** Overflow every 250 us! */
+/** Overflow every 5ms! */
 void TMR2_ISR() {
-    volatile static word tmr6_ovf1 = 0; /* Counter for Timer6 overflow */
-    volatile byte tmp[2];
-    
+    static byte tmr6_ovf1 = 0; /* Counter for Timer6 overflow */
+    byte tmp[2];
+
     TMR2 = 6;
 
     /* Clock clocking */
-    if (tmr6_ovf1++ >= 4000) // 250us*4000 = 1 ms have passed
+    if (++tmr6_ovf1 >= 200) // 5ms*200 = 1 s have passed
     {
         tmr6_ovf1 = 0;
         uptime_s++; /* Increase clock by one second */
 
         tp = gmtime(&uptime_s); /* Re-populates tm time.h struct */
-        
+
         /* DIRTY Workaround to fix itoa output when number is only one cypher. */
         if (tp->tm_sec < 10) {
             itoa(tmp, tp->tm_sec, 10);
             c_digits[3] = tmp[1];
             c_digits[2] = '0';
-        } else
+        } else {
             itoa(tmp, tp->tm_sec, 10);
-
+            c_digits[3] = tmp[1];
+            c_digits[2] = tmp[0];
+        }
         if (tp->tm_hour < 10) {
             itoa(tmp, tp->tm_hour, 10);
             c_digits[1] = tmp[1];
@@ -163,7 +169,7 @@ void TMR2_ISR() {
             itoa(c_digits, tp->tm_hour, 10);
         /* END of DIRTY */
     }
-    
+
     PIR1bits.TMR2IF = 0;
 }
 
