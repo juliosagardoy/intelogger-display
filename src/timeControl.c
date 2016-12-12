17590a8e1674c9309689db08395768b7988b397d
juliosagardoy <js387@gmx.de>
2016-12-12 19:12:01 +0100

    Fixed refresh, now working. Comments are to be corrected

--- src/timeControl.c
+++ src/timeControl.c
@@ -23,11 +23,11 @@ init_tmr6() {
     active_digit = 0;
 
     /* Configure overflow: Fcy=1MHz; Tcy=1us; incTMR=Tcy/2
-     * 1us * 1 * 10 = 10us per tick */
-    T6CONbits.T6CKPS = 0b01; // Prescaler @ 1:1
-    T6CONbits.T6OUTPS = 0b0101; // Postscaler @ 1:4
+     * 1us * 5 * 4 = 24us per tick */
+    T6CONbits.T6CKPS = 0b01; // Prescaler @ 1:4
+    T6CONbits.T6OUTPS = 0b0001; // Postscaler @ 1:2
 
-    TMR6 = 6; // Then irq ea. 250*10us = 2.5ms (*4 char = 10ms = 100Hz)
+    TMR6 = 0; // Then irq ea. 256*8us = 2048us (*4 char = 122Hz)
     PIE3bits.TMR6IE = 1;
     T6CONbits.TMR6ON = 1;
 }
@@ -37,11 +37,11 @@ init_tmr6() {
  */
 static void
 init_tmr4() {
-    /* Configure overflow:cy=1MHz; Tcy=1us; incTMR=Tcy/2
-     * 0,5us * 64presc = 32us/tick 
-     * 32us/tick * 256 = 0,008192 s to overflow
+    /* Configure overflow:cy=1MHz; Tcy=1us; incTMR=Tcy/2--------------------TPOOPOOODD
+     * 1us * 4presc = 4us/tick 
+     * 4us/tick * 256 = 1,024 ms to overflow
      */
-    T4CONbits.T4CKPS = 0b11; /* Prescaler @ 1:64 */
+    T4CONbits.T4CKPS = 0b01; /* Prescaler @ 1:64 */
     PIE3bits.TMR4IE = 1;
 
     T4CONbits.TMR4ON = 1;
@@ -70,16 +70,17 @@ void
 init_ccp() {
     TRISBbits.TRISB0 = 1; /* Temporarily disable output to avoid glitches */
     CCPTMRS0bits.C4TSEL = 0b01; /* CCP4 is based off Timer4 in PWM mode */
-    PR4 = 127; /* PWM Period = TMR2 till ovf/2 = 0.008192 s */
+    PR4 = 256; /* PWM Period = TMR4 till ovf = 0.001024 s */
 
      /* Select duty cycle 2-MSb (10) and PWM mode (11xx) */
     //CCP4CON = 0b101100;
-CCP4CON = 0b111100;
+    CCP4CON = 0b011100;
     /* 2 LSb in CCP4CON */
     //CCPR4L = 0b000000; /* duty cycle 8-MSb */
-CCPR4L = 0b111111; /* duty cycle 8-MSb */
+    CCPR4L = 0b111111; /* duty cycle 8-MSb */
     init_tmr4();
-
+    
+    TMR4 = 0;
     PIE3bits.CCP4IE = 1;
     TRISBbits.TRISB0 = 0;
 }
@@ -133,7 +134,7 @@ incr_active_digit();
     }
     /* At each iteration, switch active digit */
     
-
+    
     PIR3bits.TMR6IF = 0;
 }
 
